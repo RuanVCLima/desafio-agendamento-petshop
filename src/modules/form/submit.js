bd08e4318a34abae19aee101ee0540c18716564d
Ruan Victor <ruanvclima@gmail.com>
2026-08-08 14:20:55 -0300

    fixing arrow down, and validating date and time

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,7 +1,25 @@
+import dayjs from "dayjs"
+
 const form = document.querySelector("form")
 const btNewSchedule = document.getElementById("bt-new-schedule")
 const modal = document.getElementById("modal")
 const closeModal = document.getElementById("close-modal")
+const personalDate = document.getElementById("date")
+const chosingDate = document.getElementById("date-schedule")
+const time = document.getElementById("time")
+
+personalDate.value = dayjs(new Date()).format("YYYY-MM-DD")
+chosingDate.value = dayjs(new Date()).format("YYYY-MM-DD")
+time.value = dayjs().format("HH:mm")
+
+personalDate.min = dayjs(new Date()).format("YYYY-MM-DD")
+chosingDate.min = dayjs(new Date()).format("YYYY-MM-DD")
+time.min = dayjs().format("HH:mm")
+
+
+function validatingTime(){
+    time.min = dayjs().format("HH:mm")
+}
 
 closeModal.addEventListener("click",(event)=>{
     event.preventDefault()
@@ -15,5 +33,6 @@ btNewSchedule.addEventListener("click",(event)=>{
 )
 form.onsubmit = (event)=>{
     event.preventDefault()
+    validatingTime()
     console.log("Enviado")
 }
